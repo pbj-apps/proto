ffeb6d909ffc86e8f23f2cc58ee8f44e4455a520
Mitch Devenport <mitch@heyrenee.co>
2021-11-11 12:35:51 -0500

    cors preflight method

--- js/heyrenee/v1/appointment_service_pb_service.d.ts
+++ js/heyrenee/v1/appointment_service_pb_service.d.ts
@@ -2,7 +2,17 @@
 // file: heyrenee/v1/appointment_service.proto
 
 import * as heyrenee_v1_appointment_service_pb from "../../heyrenee/v1/appointment_service_pb";
+import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
 import {grpc} from "@improbable-eng/grpc-web";
+
+type AppointmentServiceCorsPreflight = {
+  readonly methodName: string;
+  readonly service: typeof AppointmentService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof google_protobuf_empty_pb.Empty;
+  readonly responseType: typeof google_protobuf_empty_pb.Empty;
+};
 
 type AppointmentServiceListAppointments = {
   readonly methodName: string;
@@ -15,6 +25,7 @@ type AppointmentServiceListAppointments = {
 
 export class AppointmentService {
   static readonly serviceName: string;
+  static readonly CorsPreflight: AppointmentServiceCorsPreflight;
   static readonly ListAppointments: AppointmentServiceListAppointments;
 }
 
@@ -50,6 +61,15 @@ export class AppointmentServiceClient {
   readonly serviceHost: string;
 
   constructor(serviceHost: string, options?: grpc.RpcOptions);
+  corsPreflight(
+    requestMessage: google_protobuf_empty_pb.Empty,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: google_protobuf_empty_pb.Empty|null) => void
+  ): UnaryResponse;
+  corsPreflight(
+    requestMessage: google_protobuf_empty_pb.Empty,
+    callback: (error: ServiceError|null, responseMessage: google_protobuf_empty_pb.Empty|null) => void
+  ): UnaryResponse;
   listAppointments(
     requestMessage: heyrenee_v1_appointment_service_pb.ListAppointmentsRequest,
     metadata: grpc.Metadata,
