5842e76632f10bfd41ef11319b8da68622927436
Mitch Devenport <mitch@heyrenee.co>
2021-11-12 07:09:46 -0500

    remove old cors

--- js/heyrenee/v1/cors_service_pb.d.ts
+++
@@ -1,7 +0,0 @@
-// package: heyrenee.v1
-// file: heyrenee/v1/cors_service.proto
-
-import * as jspb from "google-protobuf";
-import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
-import * as google_api_annotations_pb from "../../google/api/annotations_pb";
-
